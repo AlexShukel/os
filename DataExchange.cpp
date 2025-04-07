7c26b831704d2d6caaf087a8c54d57bc86459aa2
Soskar1 <oskar.vistorskis@gmail.com>
2025-04-07 17:37:07 +0300

    Reading hdd.txt file. Loading process to RAM by file name

--- DataExchange.cpp
+++ DataExchange.cpp
@@ -23,30 +23,43 @@ void DataExchange::xchg() {
     }
 
     if (sourceObject == EXTERNAL) {
-        std::ifstream file(path);
+        std::ifstream hdd("hdd.txt");
 
-        if (!file) {
-            std::cerr << "ERROR: failed to open file " << path << std::endl;
+        if (!hdd) {
+            std::cerr << "ERROR: failed to open hdd.txt" << std::endl;
             exit(1);
         }
 
         // Move to the sourcePointer
-        file.seekg(sourcePointer.toInteger(), std::ios::cur);
+        hdd.seekg(sourcePointer.toInteger(), std::ios::cur);
 
         if (destinationObject == MEMORY) {
             std::string line;
             int i = destinationPointer.toInteger();
-            while (std::getline(file, line)) {
-                if (line.size() != WORD_SIZE) {
-                    throw std::runtime_error("ERROR: each line on external storage must contain exactly one word.");
+            while (std::getline(hdd, line)) {
+                while (line != "@FILE0") {
+                    std::getline(hdd, line);
                 }
 
-                memory->writeWord(Word(line), i);
-                ++i;
+                std::getline(hdd, line);
+                if (line != path)
+                    continue;
+
+                while (line != "@END00") {
+                    std::getline(hdd, line);
+
+                    if (line.size() != WORD_SIZE) {
+                        throw std::runtime_error("ERROR: each line on external storage must contain exactly one word.");
+                    }
+    
+                    memory->writeWord(Word(line), i);
+                    ++i;
+                }
+                break;
             }
         }
 
-        file.close();
+        hdd.close();
     }
 }
 
